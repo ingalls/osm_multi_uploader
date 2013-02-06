67ead1682ab8e37438934d97ff352b89c79e8e72
MaltaProject <nicholas.ingalls@gmail.com>
2013-02-06 18:28:27 -0400

    Initial Commit

---
+++ multi_uploader.py
@@ -0,0 +1,49 @@
+# This program serves as an interface with the bulk
+# uploader program. It allows one to specify a directory
+# containing multiple osm files and have them all uploaded at
+# once. Please use this program carefully and reach a
+# a community consensus before massive imports.
+# ~ingalls
+
+import os
+
+print "OSM Multi-Uploader v0.1"
+print "Please make sure that you know what you are doing..."
+print "'nuff said! \n"
+print "Directory (Press enter for current)"
+fileLoc = raw_input(":")
+
+if fileLoc == "":
+	fileLoc = ""
+else:
+	os.chdir(fileLoc)
+
+print "Username"
+username = raw_input(":")
+
+print "Password"
+password = raw_input(":")
+
+print "Changeset Comment"
+comment = raw_input(":")
+
+print "\nAre you sure you wish to continue?"
+check = raw_input("type 'yes' to continue \n:")
+if check != "yes":
+	exit()
+
+fileList = list()
+
+for files in os.listdir("."):
+    if files.endswith(".osm"):
+        fileList.append(files)
+
+listNum = len(fileList)
+listNum = listNum - 1
+while listNum >= 0:
+	print "--- Converting: " + fileList[listNum] + "---"
+	os.system("python3 osm2change.py " + fileList[listNum])
+	newFile = fileList[listNum].replace(".osm", ".osc")
+	os.system("python3 upload.py -u " + username + " -p " + password + " -m " + comment + " -t -c yes " + newFile )
+	listNum = listNum - 1
+
---
+++ revert.pl
@@ -0,0 +1,71 @@
+
+<!-- saved from url=(0064)http://svn.openstreetmap.org/applications/utils/revert/revert.pl -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">#!/usr/bin/perl
+
+# Adapter script for Revert.pm module
+# exports Revert.pm functionality for command line use.
+
+use strict;
+use warnings;
+use Revert;
+use Changeset;
+
+my $revert_creation = 1;
+
+if (scalar(@ARGV) &lt; 1 || scalar(@ARGV) &gt; 2)
+{
+    print &lt;&lt;EOF;
+usage: $0 &lt;changeset_to_undo&gt; [ &lt;current_changeset&gt; | &lt;comment&gt;]
+
+where 
+  changeset_to_undo : is the id of the changeset you want to revert,
+     or the minus sign (-) if you want to read a .osc file from stdin
+  current_changeset : is the id of a currently open changeset under
+     which this action will run.
+     If this is given, the changeset will not be closed in order
+     to allow you to run multiple operations on the same changeset.
+     If unset, a new changeset will be created and closed afterwards.
+  comment : the comment to use when closing the changeset (must not
+     be numeric lest it would be interpreted as a changeset id).
+EOF
+    exit;
+}
+
+my ($undo_cs, $current_cs_or_comment) = @ARGV;
+my $do_close = 0;
+
+my $current_cs;
+my $comment; 
+
+if ($undo_cs eq "-")
+{
+    $undo_cs = "";
+    $undo_cs .= $_ while(&lt;&gt;);
+}
+
+# what have we got, changeset or comment?
+if ($current_cs_or_comment =~ /^\d+$/)
+{
+    $current_cs = $current_cs_or_comment;
+}
+else
+{
+    $comment = $current_cs_or_comment;
+    $comment = "reverting changeset $undo_cs" if ($comment eq "");
+    $current_cs = Changeset::create($comment);
+    $do_close = 1;
+}
+
+if (defined($current_cs))
+{
+    if (Revert::revert($undo_cs, $current_cs, $revert_creation)) 
+    {
+        if ($do_close)
+        {
+            $comment = "reverted changeset $undo_cs" if ($comment eq "");
+            Changeset::close($current_cs, $comment);
+        }
+    }
+}
+
+</pre></body><style type="text/css"></style></html>
